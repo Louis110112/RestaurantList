5484932434b64807dc1cd075d0c7c123ab30d6c0
louis93507@gmail.com <louis93507@gmail.com>
2024-10-08 00:21:51 +0800

    express-handlebarsAsviewEngine

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3009
+
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
+app.use(express.static('public'))
+
 
 app.get('/', (req, res) => {
     res.redirect('/restaurantlist')
 })
 
 app.get('/restaurantlist', (req, res) => {
-    res.send('LIST')
+    res.render('index')
+})
+
+app.get('/restaurantlist/:id', (req, res) => {
+    const id = req.params.id
+    res.send(`read restaurantlist${id}`)
 })
 
 app.listen(port, () => {
