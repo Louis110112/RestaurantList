2cba73e6ea404dc82807957c62cb3e7790e086c4
louis93507@gmail.com <louis93507@gmail.com>
2024-10-08 15:20:58 +0800

    AddListingPage

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
-const port = 3009
+const port = 8080
+const restaurantlist = require('./public/jsons/restaurant.json').results
+const BASE_IMG_URL = 'https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/'
 
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', './views');
-
 app.use(express.static('public'))
-
 
 app.get('/', (req, res) => {
     res.redirect('/restaurantlist')
 })
 
 app.get('/restaurantlist', (req, res) => {
-    res.render('index')
+    res.render('index', { restaurantlist, BASE_IMG_URL },)
 })
 
 app.get('/restaurantlist/:id', (req, res) => {
