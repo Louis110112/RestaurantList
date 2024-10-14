b8333951ea77d881d10e0a6dfe9255d318a84597
louis93507@gmail.com <louis93507@gmail.com>
2024-10-14 15:22:28 +0800

    SearchFuntion

--- app.js
+++ app.js
@@ -15,8 +15,16 @@ app.get('/', (req, res) => {
 })
 
 app.get('/restaurantlist', (req, res) => {
-    const keyword = req.query.search || ""
-    const matchedRes = restaurantlist.filter((RES) => RES.name.toLowerCase().includes(keyword.toLowerCase()))
+    const keyword = req.query.search
+    // const matchedRes = restaurantlist.filter((RES) => RES.name.toLowerCase().includes(keyword.toLowerCase()))
+    const matchedRes = keyword ? restaurantlist.filter((RES) =>
+        Object.values(RES).some((property) => {
+            if (typeof property === 'string') {
+                return property.toLowerCase().includes(keyword.toLowerCase())
+            }
+            return false
+        })
+    ) : restaurantlist
     res.render('index', { restaurantlist: matchedRes, BASE_IMG_URL, keyword })
 })
 
